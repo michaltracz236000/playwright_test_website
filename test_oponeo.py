79bee3b5054299f93e72394f8356832e973a9a1f
Michal Tracz <michaltracz00@gmail.com>
2024-08-16 10:52:25 +0200

    add test choose tyre size

--- test_oponeo.py
+++ test_oponeo.py
@@ -5,5 +5,20 @@ import time
 def test_decline_cookies(page: Page):
     page.goto("https://www.oponeo.pl")
     page.locator("#consentsBar > div > div > span.reject.button.primary.md.solid").click()
-    time.sleep(5)
+    time.sleep(1)
     expect(page.get_by_text("Odrzuć")).to_be_hidden()
+
+def test_choose_tyre_size(page: Page):
+    page.goto("https://www.oponeo.pl")
+    page.locator("#consentsBar > div > div > span.reject.button.primary.md.solid").click()
+    time.sleep(1)
+    page.locator("#_carTires_ctTS_ddlDimWidth").select_option("215")
+    time.sleep(1)
+    page.locator("#_carTires_ctTS_ddlDimRatio").select_option("55")
+    time.sleep(1)
+    page.locator("#_carTires_ctTS_ddlDimDiameter").select_option("17")
+    time.sleep(1)
+    expect(page.locator("#_carTires_ctTS_ddlDimWidth")).to_have_value("215")
+    expect(page.locator("#_carTires_ctTS_ddlDimRatio")).to_have_value("55")
+    expect(page.locator("#_carTires_ctTS_ddlDimDiameter")).to_have_value("17")
+    
