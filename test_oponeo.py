11673a41c1bb8800ebd69b283c77d0e7865896d5
Michal Tracz <michaltracz00@gmail.com>
2024-08-16 10:30:13 +0200

    add test decline cookies

--- test_oponeo.py
+++ test_oponeo.py
@@ -2,9 +2,8 @@ import re
 from playwright.sync_api import Page, expect
 import time
 
-def test_has_title(page: Page):
+def test_decline_cookies(page: Page):
     page.goto("https://www.oponeo.pl")
-    time.sleep(5)
-
     page.locator("#consentsBar > div > div > span.reject.button.primary.md.solid").click()
     time.sleep(5)
+    expect(page.get_by_text("Odrzuć")).to_be_hidden()
