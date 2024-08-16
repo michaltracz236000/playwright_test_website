4e7fd72321fa4903387e71dbcfd1cae3a5a2ad0a
Michal Tracz <michaltracz00@gmail.com>
2024-08-16 11:02:53 +0200

    test: add tests for choosing tyre type

--- test_oponeo.py
+++ test_oponeo.py
@@ -22,3 +22,37 @@ def test_choose_tyre_size(page: Page):
     expect(page.locator("#_carTires_ctTS_ddlDimRatio")).to_have_value("55")
     expect(page.locator("#_carTires_ctTS_ddlDimDiameter")).to_have_value("17")
     
+def test_choose_tyre_type1(page: Page):
+    page.goto("https://www.oponeo.pl")
+    page.locator("#consentsBar > div > div > span.reject.button.primary.md.solid").click()
+    time.sleep(1)
+    page.locator("#forSize > div:nth-child(2) > div.chooseBox.producer > div").click()
+    time.sleep(1)
+    page.locator("#forSize > div:nth-child(2) > div.chooseBox.producer.visible > div > div.options > div > div.producersGroup > div.groupItem.groupPremium > div.groupTitle > label > span > span").click()
+    time.sleep(1)
+    page.locator("#_carTires_ctTS_olProducers_lbAck0").click()
+    expect(page.locator("#forSize > div:nth-child(2) > div.chooseBox.producer > div > div.dropdown > span")).to_have_text("9 wybranych")
+
+def test_choose_tyre_type2(page: Page):
+    page.goto("https://www.oponeo.pl")
+    page.locator("#consentsBar > div > div > span.reject.button.primary.md.solid").click()
+    time.sleep(1)
+    page.locator("#forSize > div:nth-child(2) > div.chooseBox.producer > div").click()
+    time.sleep(1)
+    page.locator("#forSize > div:nth-child(2) > div.chooseBox.producer.visible > div > div.options > div > div.producersGroup > div.groupItem.groupMedium > div.groupTitle > label > span > span").click()
+    time.sleep(1)
+    page.locator("#_carTires_ctTS_olProducers_lbAck0").click()
+    expect(page.locator("#forSize > div:nth-child(2) > div.chooseBox.producer > div > div.dropdown > span")).to_have_text("12 wybranych")
+
+
+def test_choose_tyre_type3(page: Page):
+    page.goto("https://www.oponeo.pl")
+    page.locator("#consentsBar > div > div > span.reject.button.primary.md.solid").click()
+    time.sleep(1)
+    page.locator("#forSize > div:nth-child(2) > div.chooseBox.producer > div").click()
+    time.sleep(1)
+    page.locator("#forSize > div:nth-child(2) > div.chooseBox.producer.visible > div > div.options > div > div.producersGroup > div.groupItem.groupCheap > div.groupTitle > label > span > span").click()
+    time.sleep(1)
+    page.locator("#_carTires_ctTS_olProducers_lbAck0").click()
+    expect(page.locator("#forSize > div:nth-child(2) > div.chooseBox.producer > div > div.dropdown > span")).to_have_text("84 wybranych")
+
